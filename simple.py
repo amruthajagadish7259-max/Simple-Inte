7c7b68072eefac438eb8995fe289259d940f2f3a
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-11-11 11:29:20 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,5 +1,18 @@
-p=1000;
-t=2;
-r=5;
-SIMPLE_ITEREST=(p*t*r)/100;
-print(f"Simple  Iterest:{SIMPLE_ITEREST}")
+import sys
+
+if len(sys.argv) == 4:
+    p = float(sys.argv[1])   # principal amount
+    t = float(sys.argv[2])   # time in years
+    r = float(sys.argv[3])   # rate of interest
+    print("User input provided")
+else:
+    p = 1000
+    t = 1
+    r = 2
+
+si = (p * t * r) / 100
+
+print("The Principal is:", p)
+print("The Time is:", t)
+print("The Rate is:", r)
+print("The Simple Interest is:", si)
